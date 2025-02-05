85c00a95cd332045c7d5fe5967150afcc4f7c4a9
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-06 04:03:40 +0700

    update cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,11 +7,17 @@ import (
 
 func ConfigCORS() echo.MiddlewareFunc {
 	config := middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:4321",
-			"https://be-shiftwave-dev.mijio.app",
-			"https://shiftwave-dev.mijio.app",
+		AllowOrigins: []string{
+			"http://localhost:4321",
+			"https://be-shiftwave-dev.mijio.app/",
+			"https://shiftwave-dev.mijio.app/",
 		},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
 	})
 	return config
 }
