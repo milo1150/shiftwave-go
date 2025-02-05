69e37649f5213090c9400028e27ec3c8996c33d9
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-06 01:28:55 +0700

    build: add cors url

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func ConfigCORS() echo.MiddlewareFunc {
 	config := middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:4321"},
+		AllowOrigins: []string{"http://localhost:4321", "https://shiftwave-dev.mijio.app"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	})
 	return config
